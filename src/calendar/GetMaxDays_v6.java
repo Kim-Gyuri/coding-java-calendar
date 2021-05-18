a60adad968a48d98cd718b697d2fcb7a6a8898a7
thumper_is_stronger <57389368+Kim-Gyuri@users.noreply.github.com>
2021-05-19 02:00:14 +0900

    Create GetMaxDays_v6.java
    if문을 추가해서 12값보다 큰 수가 입력되면, continue로 다음 입력을 받을 수 있게 반복문을 다시 돌려 에러를 막을 수 있다.

---
+++ src/calendar/GetMaxDays_v6.java
@@ -0,0 +1,32 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class GetMaxDays_v6 {
+
+    private final static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String PROMPT ="cal>";
+
+        int month =1;
+
+        while (true) {
+            System.out.println("달을 입력하시오.");
+            System.out.print(PROMPT);
+            month = sc.nextInt();
+            if(month == -1) {
+                break;
+            }
+            if(month >12) {
+                continue;
+            }
+            System.out.printf( "%d월은 %d일까지 있습니다.\n", month,MAX_DAYS[month-1]);
+        }
+        System.out.println("Bye~");
+        sc.close();
+
+    }
+}
